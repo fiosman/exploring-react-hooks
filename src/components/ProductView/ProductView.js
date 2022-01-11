9dff7327faf9df1021dd31202ce79030e8113228
fiosman <fares.osman@outlook.com>
2022-01-10 20:58:10 -0500

    Finish Phase I

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,38 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
-import './ProductView.css'
+import "./ProductView.css";
 
 function ProductView({ products }) {
+  const [isSideOpen, setIsSideOpen] = useState(true);
 
-    // TODO: Replace with state variable
-    const sideOpen = true;
-
-    return (
-        <div className="product-view">
-            <div className="product-main-area">
-                <h1>Products</h1>
-                <div className="product-list">
-                    {products.map(item =>
-                        <ProductListItem
-                            key={item.id}
-                            product={item}
-                            onClick={() => console.log('SELECT PRODUCT', item)}
-                        />
-                    )}
-                </div>
-            </div>
-            <div className="product-side-panel">
-                <div className="product-side-panel-toggle-wrapper">
-                    <div className="product-side-panel-toggle"
-                         onClick={() => console.log('TOGGLE SIDE PANEL')}>
-                        {sideOpen ? '>' : '<'}
-                    </div>
-                </div>
-                <ProductDetails visible={sideOpen} />
-            </div>
+  return (
+    <div className="product-view">
+      <div className="product-main-area">
+        <h1>Products</h1>
+        <div className="product-list">
+          {products.map((item) => (
+            <ProductListItem
+              key={item.id}
+              product={item}
+              onClick={() => console.log("SELECT PRODUCT", item)}
+            />
+          ))}
         </div>
-    );
+      </div>
+      <div className="product-side-panel">
+        <div className="product-side-panel-toggle-wrapper">
+          <div className="product-side-panel-toggle" onClick={() => setIsSideOpen(!isSideOpen)}>
+            {isSideOpen ? ">" : "<"}
+          </div>
+        </div>
+        <ProductDetails visible={isSideOpen} />
+      </div>
+    </div>
+  );
 }
 
 export default ProductView;
